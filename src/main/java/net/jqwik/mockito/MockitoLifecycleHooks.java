85d1092118a1c1ee4d64ac6803f652e47acda01b
AGustafson <agustafson@atlassian.com>
2024-10-24 17:30:41 +1100

    MockitoLifecycleHooks: add javadoc

--- src/main/java/net/jqwik/mockito/MockitoLifecycleHooks.java
+++ src/main/java/net/jqwik/mockito/MockitoLifecycleHooks.java
@@ -14,6 +14,36 @@ import org.mockito.MockitoAnnotations;
 
 import java.util.List;
 
+/**
+ * Provides {@link net.jqwik.api.lifecycle.LifecycleHook}s to enable usage of Mockito with jqwik.
+ * <br/>
+ * <ul>
+ *   <li>Creates mocks for any fields with Mockito annotations such as {@link org.mockito.Mock} or {@link org.mockito.Spy}.</li>
+ *   <li>Resets all mocks between each try, whether those mocks were created programmatically (eg: via calls to {@code Mockito.mock()})
+ *   or via annotations.</li>
+ * </ul>
+ *
+ * <pre>{@code
+ * import net.jqwik.api.lifecycle.AddLifecycleHook;
+ * import org.mockito.InjectMocks;
+ * import org.mockito.Mock;
+ * import org.mockito.Mockito;
+ * import org.mockito.Spy;
+ * @AddLifecycleHook(MockitoLifecycleHooks.class)
+ * class OrderServiceTest {
+ *     @Mock
+ *     private OrderRepository orderRepository;
+ *     private OrderRepository orderRepository2 = Mockito.mock();
+ *     private LoggingService loggingService = Mockito.spy(new ConsoleLoggingServiceImpl());
+ *     @Spy
+ *     private ConsoleLoggingServiceImpl loggingService2;
+ *     @InjectMock
+ *     private OrderService orderService;
+ * }
+ * }</pre>
+ *
+ * @see net.jqwik.api.lifecycle.AddLifecycleHook
+ */
 public class MockitoLifecycleHooks implements AroundPropertyHook, AroundTryHook {
     private Object[] mocks;
 
@@ -23,19 +53,19 @@ public class MockitoLifecycleHooks implements AroundPropertyHook, AroundTryHook 
     }
 
     @Override
-    public PropertyExecutionResult aroundProperty(PropertyLifecycleContext context, PropertyExecutor property)
+    public PropertyExecutionResult aroundProperty(PropertyLifecycleContext context, PropertyExecutor propertyExecutor)
             throws Throwable {
         try (AutoCloseable ignored = MockitoAnnotations.openMocks(context.testInstance())) {
             // finds all mocked fields within the test instance object
             mocks = MockFinder.getMocks(context.testInstance()).toArray(new Object[0]);
-            return property.execute();
+            return propertyExecutor.execute();
         }
     }
 
     @Override
-    public TryExecutionResult aroundTry(TryLifecycleContext context, TryExecutor aTry, List<Object> parameters) {
+    public TryExecutionResult aroundTry(TryLifecycleContext context, TryExecutor tryExecutor, List<Object> parameters) {
         try {
-            return aTry.execute(parameters);
+            return tryExecutor.execute(parameters);
         } finally {
             Mockito.reset(mocks);
         }
