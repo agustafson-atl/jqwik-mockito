9f976c3158015f76419c5a01b493399504a95177
AGustafson <agustafson@atlassian.com>
2024-11-05 09:55:20 +1100

    Fix javadocs

--- src/main/java/net/jqwik/mockito/MockitoLifecycleHooks.java
+++ src/main/java/net/jqwik/mockito/MockitoLifecycleHooks.java
@@ -16,31 +16,30 @@ import java.util.List;
 
 /**
  * Provides {@link net.jqwik.api.lifecycle.LifecycleHook}s to enable usage of Mockito with jqwik.
- * <br/>
  * <ul>
  *   <li>Creates mocks for any fields with Mockito annotations such as {@link org.mockito.Mock} or {@link org.mockito.Spy}.</li>
  *   <li>Resets all mocks between each try, whether those mocks were created programmatically (eg: via calls to {@code Mockito.mock()})
  *   or via annotations.</li>
  * </ul>
  *
- * <pre>{@code
+ * <pre>
  * import net.jqwik.api.lifecycle.AddLifecycleHook;
  * import org.mockito.InjectMocks;
  * import org.mockito.Mock;
  * import org.mockito.Mockito;
  * import org.mockito.Spy;
- * @AddLifecycleHook(MockitoLifecycleHooks.class)
+ * {@literal @}AddLifecycleHook(MockitoLifecycleHooks.class)
  * class OrderServiceTest {
- *     @Mock
+ *     {@literal @}Mock
  *     private OrderRepository orderRepository;
  *     private OrderRepository orderRepository2 = Mockito.mock();
  *     private LoggingService loggingService = Mockito.spy(new ConsoleLoggingServiceImpl());
- *     @Spy
+ *     {@literal @}Spy
  *     private ConsoleLoggingServiceImpl loggingService2;
- *     @InjectMock
+ *     {@literal @}InjectMock
  *     private OrderService orderService;
  * }
- * }</pre>
+ * </pre>
  *
  * @see net.jqwik.api.lifecycle.AddLifecycleHook
  */
